dad25dcaa816aea24b2b97d932fcbac14c015de0
nan204q <nan10052004@gmail.com>
2025-10-10 14:20:28 +0700

    feat: Setup basic routing for homepage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+// src/pages/HomePage.jsx
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
---
+++ src/routes/index.jsx
@@ -0,0 +1,14 @@
+// src/routes/index.jsx
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from '../pages/HomePage';
+
+const AppRouter = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  );
+};
+
+export default AppRouter;
