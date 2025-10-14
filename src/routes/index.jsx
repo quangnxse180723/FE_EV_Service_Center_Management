a55894b7053a1f11cfd25af3b62fa2d5f3863ad4
longpro344 <longddse180296@fpt.edu.vn>
2025-10-14 22:46:38 +0700

    Thêm chức năng Admin Dashboard

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,22 @@
 // src/routes/index.jsx
+
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+
 import HomePage from '../pages/HomePage';
+import LoginPage from '../modules/auth/LoginPage';
+import RegisterPage from '../modules/auth/RegisterPage';
+import BookingPage from '../modules/customer/pages/BookingPage';
+import DashboardPage from '../modules/admin/pages/DashboardPage';
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/booking" element={<BookingPage />} />
+      <Route path="/admin/dashboard" element={<DashboardPage />} />
     </Routes>
   );
 };
